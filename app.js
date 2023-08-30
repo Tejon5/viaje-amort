0b34dcef79f299222132f2f2e093dc23a9952b0c
Tejon5 <juan.villalba08@gmail.com>
2023-08-30 18:34:48 -0300

    improvements for USA wc 2

--- app.js
+++ app.js
@@ -1,43 +1,4 @@
-/*
-const cero = 0;
-const countDate = new Date("Aug 30, 2023 21:30:00");
 
-
-const countdown = () => {
-  let interval = setInterval(() => {
-    const now = new Date();
-    let gap = countDate - now;
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
- 
-    const textDay = Math.floor(gap / day);
-    document.querySelector(".day").innerHTML = textDay;
-    const textHour = Math.floor((gap % day) / hour);
-    document.querySelector(".hour").innerHTML = textHour;
-
-    const textMinutes = Math.floor((gap % hour) / minute);
-    document.querySelector(".minute").innerHTML = textMinutes;
-
-    const textSeconds = Math.floor((gap % minute) / second);
-    document.querySelector(".second").innerHTML = textSeconds;
-    if (gap <= 0) {
-      clearInterval(interval);
-      gap = 0;
-      document.querySelector(".day").innerHTML = cero;
-      document.querySelector(".hour").innerHTML = cero;
-      document.querySelector(".minute").innerHTML = cero;
-      document.querySelector(".second").innerHTML = cero;
-    }
-  },1000);
-
- 
-};
-
-countdown();
-*/
 
 const obtenerPais = (callback, cb2) => {
   fetch('https://ipapi.co/json/')
@@ -60,7 +21,7 @@ obtenerPais(pais => {
     countdown(countDate)
   }
   if(pais==='United States'){
-    let countDate = new Date("Aug 30, 2023 19:30:00");
+    let countDate = new Date("Aug 30, 2023 17:30:00");
 countdown(countDate)
 }
   else {
@@ -105,7 +66,6 @@ const countdown = (countDate) => {
       document.querySelector(".minute").innerHTML = 'C';
       document.querySelector(".second").innerHTML = 'A';
 
-     /* alert('BOCAA BOCAAA BOCA')*/
     }
   },1000);
 
