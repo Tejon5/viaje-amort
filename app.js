c79a7e480bd958ecdb2494228d2848a2ca68e865
Tejon5 <juan.villalba08@gmail.com>
2023-08-30 01:07:45 -0300

    code changes and fix

--- app.js
+++ app.js
@@ -1,44 +1,39 @@
-const countdown = () =>{
-    //con esta funcion determinamos la fecha de referancia sobre la cual se va a determinar el countdown
-    /*const countDate = new Date('Aug 30, 2023 21:30:00').getTime()*/
-    const countDate = new Date('Aug 30, 2023 21:30:00');
 
-    //con esta funcion determinamos la fecha de ahora
-    /*const now = new Date().getTime()*/
+const cero = 0;
+const countDate = new Date("Aug 30, 2023 21:30:00");
+
+
+const countdown = () => {
+  let interval = setInterval(() => {
     const now = new Date();
-
-    //El gap es la diferencia entre los valores arriba preestablecidos para que se ejecute el countdown
-    const gap = countDate-now
-   
-
-
-    // se definen las variables propias para manejar el tiempo
+    let gap = countDate - now;
     const second = 1000;
-    const minute = second*60;
-    const hour = minute*60;
-    const day = hour*24;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-    //Calculamos lo que va a mostrar
-    const textDay = Math.floor(gap/day);
-    document.querySelector('.day').innerHTML = textDay;
+ 
+    const textDay = Math.floor(gap / day);
+    document.querySelector(".day").innerHTML = textDay;
+    const textHour = Math.floor((gap % day) / hour);
+    document.querySelector(".hour").innerHTML = textHour;
 
-    //esta es la manera en que se debe contemplar como se debe dividir para tener una fecha determinada
-    //se toma el valor restante y luego se lo divide por las horas
-    const textHour = Math.floor((gap%day)/hour);
-    document.querySelector('.hour').innerHTML = textHour;
+    const textMinutes = Math.floor((gap % hour) / minute);
+    document.querySelector(".minute").innerHTML = textMinutes;
 
-    const textMinutes = Math.floor((gap%hour)/minute);
-    document.querySelector('.minute').innerHTML = textMinutes;
+    const textSeconds = Math.floor((gap % minute) / second);
+    document.querySelector(".second").innerHTML = textSeconds;
+    if (gap <= 0) {
+      clearInterval(interval);
+      gap = 0;
+      document.querySelector(".day").innerHTML = cero;
+      document.querySelector(".hour").innerHTML = cero;
+      document.querySelector(".minute").innerHTML = cero;
+      document.querySelector(".second").innerHTML = cero;
+    }
+  },1000);
 
-    const textSeconds = Math.floor((gap%minute)/second);
-    document.querySelector('.second').innerHTML =  textSeconds;
+ 
+};
 
-}
-
-
-
-setInterval(() => {
-    countdown();
-}, 1000);
-
-
+countdown();
