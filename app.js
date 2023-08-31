472e5d91467b573fcabfd00f2ee7465f6310e434
Tejon5 <juan.villalba08@gmail.com>
2023-08-31 00:04:35 -0300

    improvements for the world

--- app.js
+++ app.js
@@ -14,18 +14,18 @@ const obtenerPais = (callback, cb2) => {
 
 obtenerPais(pais => {
   if (pais==='Argentina'||pais==='Colombia'||pais==='Uruguay'|| pais==='Paraguay' || pais==='Brasil'||pais==='Ecuador' ) {
-    let countDate = new Date("Aug 30, 2023 21:30:00");
+    let countDate = new Date("Sep 3, 2023 18:30:00");
     countdown(countDate)
   }else if(pais==='Australia'){
-        let countDate = new Date("Aug 31, 2023 10:00:00");
+        let countDate = new Date("Sep 4, 2023 7:00:00");
     countdown(countDate)
   }
   else if(pais==='United States'){
-    let countDate = new Date("Aug 30, 2023 17:30:00");
+    let countDate = new Date("Sep 3, 2023 14:30:00");
 countdown(countDate)
 }
   else {
-    let countDate = new Date("Aug 31, 2023 02:30:00");
+    let countDate = new Date("Sep 3, 2023 23:30:00");
     countdown(countDate)
   }
 });
