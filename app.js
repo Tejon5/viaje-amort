fa09813f7ba02365bbb366c950747b99a1594092
Tejon5 <juan.villalba08@gmail.com>
2024-02-02 09:49:10 -0300

    first match 3

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const cuantoescounter = (utc2, utc3) => {
 obtenerPais2(utc => {
     let utc2 = Number(utc)  
     cuantoescounter(utc2,utc3)
-    let countDate = new Date("Feb 1, 2024 19:00:00").getTime()/*+utc2*/;/**17'30 */
+    let countDate = new Date("Feb 5, 2024 19:15:00").getTime()/*+utc2*/;/**17'30 */
     countdown(countDate,result,state)
 });
 
