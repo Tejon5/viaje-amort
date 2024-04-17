3db2c0185a9a07d7714a1f9876400b98aae227a8
Tejon5 <juan.villalba08@gmail.com>
2024-04-16 21:35:09 -0300

    torneo local vs river

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const cuantoescounter = (utc2, utc3) => {
 obtenerPais2(utc => {
     let utc2 = Number(utc)  
     cuantoescounter(utc2,utc3)
-    let countDate = new Date("Apr 16 2024 19:30:00").getTime()/*+utc2*/;/**17'30 */
+    let countDate = new Date("Apr 20 2024 19:30:00").getTime()/*+utc2*/;/**17'30 */
     countdown(countDate,result,state)
 });
 
