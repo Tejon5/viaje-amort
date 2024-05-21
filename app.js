2ae4e1152d6d42d36c59aedcbed5dd6b548dd710
Tejon5 <juan.villalba08@gmail.com>
2024-05-20 22:15:37 -0300

    liga local2

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const cuantoescounter = (utc2, utc3) => {
 obtenerPais2(utc => {
     let utc2 = Number(utc)  
     cuantoescounter(utc2,utc3)
-    let countDate = new Date("May 19 2024 20:00:00").getTime()/*+utc2*/;/**17'30 */
+    let countDate = new Date("May 25 2024 20:00:00").getTime()/*+utc2*/;/**17'30 */
     countdown(countDate,result,state)
 });
 
