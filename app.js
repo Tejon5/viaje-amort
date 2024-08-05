f6840a8ee7b82144598d5db78648cdeb13240da8
Tejon5 <juan.villalba08@gmail.com>
2024-08-05 19:39:24 -0300

    fecha 10

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const cuantoescounter = (utc2, utc3) => {
 obtenerPais2(utc => {
     let utc2 = Number(utc)  
     cuantoescounter(utc2,utc3)
-    let countDate = new Date("Aug 04 2024 17:30:00").getTime()/*+utc2*/;/**17'30 */
+    let countDate = new Date("Aug 10 2024 18:45:00").getTime()/*+utc2*/;/**17'30 */
     countdown(countDate,result,state)
 });
 
