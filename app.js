e3fe42f559fa61bf069db544e108309d79c78ade
Tejon5 <juan.villalba08@gmail.com>
2024-08-12 10:04:18 -0300

    sudamericana ida

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const cuantoescounter = (utc2, utc3) => {
 obtenerPais2(utc => {
     let utc2 = Number(utc)  
     cuantoescounter(utc2,utc3)
-    let countDate = new Date("Aug 10 2024 18:45:00").getTime()/*+utc2*/;/**17'30 */
+    let countDate = new Date("Aug 15 2024 21:30:00").getTime()/*+utc2*/;/**17'30 */
     countdown(countDate,result,state)
 });
 
